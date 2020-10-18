12b5efb0770aae5424f9f73fd25b00ee54131514
“shenjian <844762167@qq.com>
2020-10-18 23:31:22 +0800

    Initial commit

---
+++ src/main/java/com/shen/sjerp/SjerpApplication.java
@@ -0,0 +1,13 @@
+package com.shen.sjerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjerpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjerpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shen/sjerp/SjerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shen.sjerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjerpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
